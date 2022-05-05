7bfdd9a100dac1b30a75c6ba697c46d4afa3f1b4
EC2 Default User <ec2-user@ip-172-31-33-31.ec2.internal>
2022-05-05 03:17:53 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,20 @@
 class ListsController < ApplicationController
   def new
+    @list=List.new
   end
 
-  def index
+  def create
+#1&2 データを受け取り新規登録するためのインスタンス作成
+    list=List.new(list_params)
+    
+    list.save
+    
+    redirect_to'/top'
   end
 
-  def show
-  end
-
-  def edit
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/new'
-  post 'lists' => 'lists#create'   #ここを追記します
+  post 'lists' => 'lists#create' 
+  get'/top'=>'homes#top'
+  #ここを追記します
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
