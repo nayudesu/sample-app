8574d3c589d52184dec1859185619a2f92769043
EC2 Default User <ec2-user@ip-172-31-33-31.ec2.internal>
2022-05-05 04:18:59 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,6 +11,10 @@ class ListsController < ApplicationController
     
     redirect_to'/top'
   end
+  
+   def index
+     @lists=List.all
+   end
 
   private
   #ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create' 
-  get'/top'=>'homes#top'
-  #ここを追記します
+  get'/top'=>'homes#top' 
+  get 'lists' => 'lists#index'#ここを追記します
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
