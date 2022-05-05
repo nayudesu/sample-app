4f85c37f4f34b5201e5ebb9baf8f7fa0a682421b
EC2 Default User <ec2-user@ip-172-31-33-31.ec2.internal>
2022-05-05 15:57:44 +0000

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get '/top'=>'homes#top' 
+  resources:lists
   get 'lists/new'
   get 'lists/edit'
   post 'lists' => 'lists#create' 
-  get '/top'=>'homes#top' 
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
- 
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
 end
